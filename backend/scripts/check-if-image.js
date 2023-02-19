8a3de5ae9143e04b7e052a4815a863e2e6d7be07
WilcoApp <hello@wilco.gg>
2023-02-19 18:13:48 +0000

    Let it sink

--- backend/scripts/check-if-image.js
+++
@@ -1,9 +0,0 @@
-
-
-const checkImage = function isImage (url) {
-    return /\.(jpg|jpeg|png|webp|avif|gif|svg)$/.test(url);
-};
-
-// console.log(checkImage("image"));
-  
-module.exports = checkImage;
